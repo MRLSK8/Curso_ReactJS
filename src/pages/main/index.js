235d3e159a32440cc24cbd32627df7267a531f28
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-01-21 19:40:08 -0300

    Updated

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,47 @@
 import React, {Component} from 'react';
 import './styles.css';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
 
 export default class Main extends Component{
     state = {
         products: [],
-        pages: 0,
+        productInfo: {},
+        page: 1
     };
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         
-        this.setState({products: response.data.docs});
-        this.setState({pages: response.data.docs.pages});
+        const {docs, ...productInfo} =  response.data;
+
+        this.setState({products: docs, productInfo, page});;
+    };
+
+    prevPage = () => {
+        const {page} = this.state;
+        console.log(page);
+        if(page === 1)return;
+        
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    };
+
+    nextPage = () => {
+        const {page, productInfo} = this.state;
+        if(page === productInfo.pages)return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
     };
 
     render(){
-        const {products} = this.state;
+        const {products, page, productInfo} = this.state;
 
         return (
             <div className='product-list'>
@@ -27,10 +49,14 @@ export default class Main extends Component{
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-
-                        <a href="">Acessar</a>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
+
+                <div className="actions">
+                    <button disabled={page === 1} onClick= {this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick= {this.nextPage}>Próximo</button>
+                </div>    
             </div>
         );
     }
